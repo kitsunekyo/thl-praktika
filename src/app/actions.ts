8028eade9eeb3708159d4b3f21d90f6486bde614
Alex Spieslechner <alexander.spieslechner@gmail.com>
2023-09-17 14:41:23 +0200

    add register + unregister for trainings

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -43,3 +43,70 @@ export async function deleteTraining(formData: FormData) {
   });
   revalidatePath("/");
 }
+
+export async function register(formData: FormData) {
+  const session = await getServerSession(authOptions);
+  const currentUser = session?.user;
+  if (!currentUser) {
+    throw new Error("must be authenticated");
+  }
+
+  const id = formData.get("id");
+  if (typeof id !== "string") {
+    throw new Error("No id provided");
+  }
+
+  const client = new PrismaClient();
+
+  const isRegisteredAlready =
+    (await client.registration.findFirst({
+      where: {
+        userId: currentUser.id,
+      },
+    })) !== null;
+
+  if (isRegisteredAlready) {
+    throw new Error("Already registered");
+  }
+
+  await client.registration.create({
+    data: {
+      trainingId: id,
+      userId: currentUser.id,
+    },
+  });
+  revalidatePath("/");
+}
+
+export async function unregister(formData: FormData) {
+  const session = await getServerSession(authOptions);
+  const currentUser = session?.user;
+  if (!currentUser) {
+    throw new Error("must be authenticated");
+  }
+
+  const id = formData.get("id");
+  if (typeof id !== "string") {
+    throw new Error("No id provided");
+  }
+
+  const client = new PrismaClient();
+  const registration = await client.registration.findFirst({
+    where: {
+      trainingId: id,
+      userId: currentUser.id,
+    },
+  });
+
+  if (!registration) {
+    throw new Error("Did not find registration");
+  }
+
+  await client.registration.delete({
+    where: {
+      id: registration.id,
+    },
+  });
+
+  revalidatePath("/");
+}
--- src/app/training-list.tsx
+++ src/app/training-list.tsx
@@ -2,58 +2,89 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { PrismaClient } from "@prisma/client";
 import { format } from "date-fns";
-import { PlusSquare, TrashIcon } from "lucide-react";
-import { deleteTraining } from "./actions";
+import { MinusSquare, PlusSquare, TrashIcon } from "lucide-react";
+import { deleteTraining, register, unregister } from "./actions";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
 export async function TrainingList() {
   const prisma = new PrismaClient();
+  const session = await getServerSession(authOptions);
+
+  const role = session?.user.role;
+
   const trainings = await prisma.training.findMany({
     include: {
       author: true,
+      registrations: true,
     },
   });
 
   return (
-    <div>
+    <section>
       <h1 className="text font-semibold mb-2">Trainings</h1>
       <ul className="space-y-2">
-        {trainings.map((training) => (
-          <li
-            key={training.id}
-            className="border border-solid rounded text-sm p-4"
-          >
-            <dl className="space-y-2">
-              <dd>{training.description}</dd>
-              <dd>{`${format(new Date(training.date), "do MMM yy")} von ${
-                training.startTime
-              } bis ${training.endTime}`}</dd>
-              <dd>0/{training.maxInterns} Praktikanten angemeldet</dd>
-            </dl>
-            <footer className="flex gap-4 pt-4 border-t mt-4 items-center">
-              <div className="flex gap-2 items-center">
-                <Avatar>
-                  {!!training.author.image && (
-                    <AvatarImage src={training.author.image} />
+        {trainings.map((training) => {
+          const isRegistered = training.registrations.some(
+            (r) => r.userId === session?.user.id
+          );
+          return (
+            <li
+              key={training.id}
+              className="border border-solid rounded text-sm p-4"
+            >
+              <dl className="space-y-2">
+                <dd>{training.description}</dd>
+                <dd>{`${format(new Date(training.date), "do MMM yy")} von ${
+                  training.startTime
+                } bis ${training.endTime}`}</dd>
+                <dd>
+                  {training.registrations.length}/{training.maxInterns}{" "}
+                  Praktikanten angemeldet
+                </dd>
+              </dl>
+              <footer className="flex gap-4 pt-4 border-t mt-4 items-center">
+                <div className="flex gap-2 items-center">
+                  <Avatar>
+                    {!!training.author.image && (
+                      <AvatarImage src={training.author.image} />
+                    )}
+                    <AvatarFallback>{training.author.name}</AvatarFallback>
+                  </Avatar>
+                  <dd>{training.author.name}</dd>
+                </div>
+                <div className="ml-auto flex gap-2 items-center">
+                  {role !== "trainer" && isRegistered ? (
+                    <form action={unregister}>
+                      <input type="hidden" name="id" value={training.id} />
+                      <Button variant="ghost" size="sm">
+                        <MinusSquare className="h-4 w-4 mr-2" />
+                        Abmelden
+                      </Button>
+                    </form>
+                  ) : (
+                    <form action={register}>
+                      <input type="hidden" name="id" value={training.id} />
+                      <Button variant="ghost" size="sm">
+                        <PlusSquare className="h-4 w-4 mr-2" />
+                        Anmelden
+                      </Button>
+                    </form>
                   )}
-                  <AvatarFallback>{training.author.name}</AvatarFallback>
-                </Avatar>
-                <dd>{training.author.name}</dd>
-              </div>
-              <div className="ml-auto flex gap-2 items-center">
-                <Button variant="ghost" size="sm">
-                  <PlusSquare className="h-4 w-4 mr-2" /> Anmelden
-                </Button>
-                <form action={deleteTraining}>
-                  <input type="hidden" name="id" value={training.id} />
-                  <Button variant="ghost" size="icon">
-                    <TrashIcon className="h-4 w-4" />
-                  </Button>
-                </form>
-              </div>
-            </footer>
-          </li>
-        ))}
+                  {role !== "user" && (
+                    <form action={deleteTraining}>
+                      <input type="hidden" name="id" value={training.id} />
+                      <Button variant="ghost" size="icon">
+                        <TrashIcon className="h-4 w-4" />
+                      </Button>
+                    </form>
+                  )}
+                </div>
+              </footer>
+            </li>
+          );
+        })}
       </ul>
-    </div>
+    </section>
   );
 }
