eac22fc0b1acd238f0889205132ead71b5855cd2
Alex Spieslechner <alexander.spieslechner@gmail.com>
2023-09-17 14:12:07 +0200

    add role badge

--- src/app/auth-header.tsx
+++ src/app/auth-header.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
 
 export function AuthHeader({ session }: { session: Session | null }) {
   return (
@@ -46,7 +47,10 @@ function User({ user }: { user: NonNullable<Session["user"]> }) {
         <AvatarFallback>{user.name}</AvatarFallback>
       </Avatar>
       <div className="text-sm">
-        <div>{user.name}</div>
+        <div className="flex gap-2 items-center">
+          {user.name}
+          {!!user.role && <Badge>{user.role}</Badge>}
+        </div>
         <div className="text-xs text-gray-500">{user.email}</div>
       </div>
     </div>
