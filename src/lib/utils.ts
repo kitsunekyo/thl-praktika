e6bb021817734a2a62618209583536e06ce80ed1
Alex Spieslechner <as@umdaschgroup-ventures.com>
2023-09-18 19:34:15 +0200

    fix avatar initials

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { format } from "date-fns";
+import { User } from "next-auth";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -15,4 +16,18 @@ export function formatTrainingDate(
     new Date(date),
     "do MMM yy",
   )} von ${startTime} bis ${endTime}`;
+}
+
+export function getInitials(user: User) {
+  const name = user.name?.split(" ");
+  if (name) {
+    const initials = name[0].charAt(0) + name[1].charAt(0);
+    return initials.toUpperCase();
+  }
+  const emailName = user.email?.split("@")[0];
+  if (emailName && emailName.length > 1) {
+    const initials = emailName.charAt(0) + emailName.charAt(1);
+    return initials.toUpperCase();
+  }
+  return "U";
 }
