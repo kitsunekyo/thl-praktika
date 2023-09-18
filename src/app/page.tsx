6af1d0197cc625c87ae7b6f243e0a635b0911f93
Alex Spieslechner <alexander.spieslechner@gmail.com>
2023-09-18 15:52:10 +0200

    dont show training list for unauthed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { getServerSession } from "next-auth";
+
+import { authOptions } from "./api/auth/[...nextauth]/route";
 import { TrainingList } from "./TrainingList";
 
 export default async function Home() {
-  return (
-    <div className="grid gap-8 md:grid-cols-2">
-      <TrainingList />
-    </div>
-  );
+  const session = await getServerSession(authOptions);
+
+  let content = <p>du musst dich anmelden</p>;
+  if (session) {
+    content = <TrainingList />;
+  }
+  return <div className="grid gap-8 md:grid-cols-2">{content}</div>;
 }
