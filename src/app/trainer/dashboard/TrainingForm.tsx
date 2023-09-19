aa337a35b2a50bc84336ef8e8ff819f60536f0ec
Alex Spieslechner <alexander.spieslechner@gmail.com>
2023-09-19 18:09:42 +0200

    add zod type coersion for maxInterns

--- src/app/trainer/dashboard/TrainingForm.tsx
+++ src/app/trainer/dashboard/TrainingForm.tsx
@@ -39,7 +39,7 @@ export const trainingSchema = z.object({
   date: z.date(),
   startTime: z.string().regex(new RegExp(/\d{1,2}:\d{1,2}/)),
   endTime: z.string().regex(new RegExp(/\d{1,2}:\d{1,2}/)),
-  maxInterns: z.string(),
+  maxInterns: z.coerce.number(),
 });
 
 export function TrainingForm() {
@@ -47,7 +47,7 @@ export function TrainingForm() {
     resolver: zodResolver(trainingSchema),
     defaultValues: {
       description: "Welpengruppe",
-      maxInterns: "3",
+      maxInterns: 3,
       date: startOfDay(new Date()),
       startTime: "08:00",
       endTime: "17:00",
@@ -58,8 +58,7 @@ export function TrainingForm() {
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit((data: z.infer<typeof trainingSchema>) => {
-          const payload = { ...data, maxInterns: parseInt(data.maxInterns) };
-          createTraining(payload);
+          createTraining(data);
         })}
         className="mx-auto space-y-8"
       >
