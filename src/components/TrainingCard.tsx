5e10d7b989003068a339e84bf3abe4601c71671e
Alex Spieslechner <as@umdaschgroup-ventures.com>
2023-09-20 23:14:17 +0200

    add mapquest distance

--- src/components/TrainingCard.tsx
+++ src/components/TrainingCard.tsx
@@ -1,16 +1,58 @@
 import { Registration, Training, User } from "@prisma/client";
+import {
+  formatDistance,
+  formatDistanceStrict,
+  formatDuration,
+  intervalToDuration,
+} from "date-fns";
 import { UserCheckIcon, UserIcon } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
-import { RegisterButton, UnregisterButton } from "@/app/register-buttons";
+import { getMe } from "@/app/(main)/profile/actions";
 import { formatTrainingDate } from "@/lib/date";
+import { getDirections } from "@/lib/mapquest";
 import { getInitials, range } from "@/lib/utils";
 
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Badge } from "./ui/badge";
 
-export function TrainingCard({
+async function getDistanceToUser(
+  user: Pick<User, "address" | "city" | "zipCode">,
+) {
+  const me = await getMe();
+  if (!me) {
+    return;
+  }
+  const myAddress = getAddress(me);
+  const userAddress = getAddress(user);
+
+  if (!myAddress || !userAddress) {
+    return;
+  }
+
+  if (process.env.NODE_ENV === "development") {
+    return { time: 90 * 60, formattedTime: "01:11:20" };
+  }
+
+  const directions = await getDirections(getAddress(me), getAddress(user));
+
+  return {
+    time: directions.route.time,
+    formattedTime: directions.route.formattedTime,
+  };
+}
+
+function secondsToDuration(seconds: number): Duration {
+  const epoch = new Date(0);
+  const secondsAfterEpoch = new Date(seconds * 1000);
+  return intervalToDuration({
+    start: epoch,
+    end: secondsAfterEpoch,
+  });
+}
+
+export async function TrainingCard({
   training,
   actions,
 }: {
@@ -20,6 +62,9 @@ export function TrainingCard({
   };
   actions?: React.ReactNode;
 }) {
+  const travelTime = await getDistanceToUser(training.author);
+  console.log({ travelTime });
+
   return (
     <div className="rounded border border-solid p-4 text-sm">
       <dl className="space-y-2">
@@ -37,15 +82,22 @@ export function TrainingCard({
               Adresse wird persönlich bekannt gegeben
             </span>
           ) : (
-            <Link
-              href={`https://www.google.com/maps/place/${getAddress(
-                training.author,
-              ).replaceAll(" ", "+")}`}
-              target="_blank"
-              className="underline hover:no-underline"
-            >
-              {getAddress(training.author)}
-            </Link>
+            <div>
+              <Link
+                href={`https://www.google.com/maps/place/${getAddress(
+                  training.author,
+                ).replaceAll(" ", "+")}`}
+                target="_blank"
+                className="underline hover:no-underline"
+              >
+                {getAddress(training.author)}{" "}
+              </Link>
+              {!!travelTime && (
+                <p className="text-xs">
+                  {formatDuration(secondsToDuration(travelTime.time))} entfernt
+                </p>
+              )}
+            </div>
           )}
         </dd>
         <dd>
@@ -101,7 +153,7 @@ function RegistrationStatus({
   );
 }
 
-function getAddress(user: User) {
+function getAddress(user: Pick<User, "address" | "city" | "zipCode">) {
   return [user.address, [user.zipCode, user.city].filter(Boolean).join(" ")]
     .filter(Boolean)
     .join(", ");
---
+++ src/lib/mapquest.ts
@@ -0,0 +1,167 @@
+export function getDirections(
+  from: string,
+  to: string,
+): Promise<DirectionResponse> {
+  const fromEncoded = encodeURIComponent(from);
+  const toEncoded = encodeURIComponent(to);
+
+  const url = new URL("https://www.mapquestapi.com/directions/v2/route");
+  const options = {
+    key: process.env.MAPQUEST_KEY,
+    from: fromEncoded,
+    to: toEncoded,
+    outFormat: "json",
+    ambiguities: "ignore",
+    routeType: "fastest",
+    doReverseGeocode: "false",
+    enhancedNarrative: "false",
+    avoidTimedConditions: "false",
+  };
+  Object.entries(options).forEach(([key, value]) => {
+    if (!value) return;
+    url.searchParams.append(key, value);
+  });
+
+  return fetch(url, {
+    cache: "force-cache",
+  }).then((res) => res.json());
+}
+
+export interface DirectionResponse {
+  route: Route;
+  info: Info;
+}
+
+export interface Info {
+  statuscode: number;
+  copyright: Copyright;
+  messages: any[];
+}
+
+export interface Copyright {
+  text: string;
+  imageUrl: string;
+  imageAltText: string;
+}
+
+export interface Route {
+  sessionId: string;
+  realTime: number;
+  distance: number;
+  time: number;
+  formattedTime: string;
+  hasHighway: boolean;
+  hasTollRoad: boolean;
+  hasBridge: boolean;
+  hasSeasonalClosure: boolean;
+  hasTunnel: boolean;
+  hasFerry: boolean;
+  hasUnpaved: boolean;
+  hasTimedRestriction: boolean;
+  hasCountryCross: boolean;
+  legs: Leg[];
+  options: Options;
+  boundingBox: BoundingBox;
+  name: string;
+  maxRoutes: string;
+  locations: Location[];
+  locationSequence: number[];
+}
+
+export interface BoundingBox {
+  ul: Lr;
+  lr: Lr;
+}
+
+export interface Lr {
+  lat: number;
+  lng: number;
+}
+
+export interface Leg {
+  index: number;
+  hasTollRoad: boolean;
+  hasHighway: boolean;
+  hasBridge: boolean;
+  hasUnpaved: boolean;
+  hasTunnel: boolean;
+  hasSeasonalClosure: boolean;
+  hasFerry: boolean;
+  hasCountryCross: boolean;
+  hasTimedRestriction: boolean;
+  distance: number;
+  time: number;
+  formattedTime: string;
+  origIndex: number;
+  origNarrative: string;
+  destIndex: number;
+  destNarrative: string;
+  maneuvers: Maneuver[];
+}
+
+export interface Maneuver {
+  index: number;
+  distance: number;
+  narrative: string;
+  time: number;
+  direction: number;
+  directionName: string;
+  signs: any[];
+  maneuverNotes: any[];
+  formattedTime: string;
+  transportMode: TransportMode;
+  startPoint: Lr;
+  turnType: number;
+  attributes: number;
+  iconUrl: string;
+  streets: string[];
+  mapUrl?: string;
+}
+
+export enum TransportMode {
+  Car = "car",
+}
+
+export interface Location {
+  street: string;
+  adminArea6: string;
+  adminArea6Type: string;
+  adminArea5: string;
+  adminArea5Type: string;
+  adminArea4: string;
+  adminArea4Type: string;
+  adminArea3: string;
+  adminArea3Type: string;
+  adminArea1: string;
+  adminArea1Type: string;
+  postalCode: string;
+  geocodeQualityCode: string;
+  geocodeQuality: string;
+  dragPoint: boolean;
+  sideOfStreet: string;
+  linkId: string;
+  unknownInput: string;
+  type: string;
+  latLng: Lr;
+  displayLatLng: Lr;
+  mapUrl: string;
+}
+
+export interface Options {
+  routeType: string;
+  enhancedNarrative: boolean;
+  doReverseGeocode: boolean;
+  narrativeType: string;
+  walkingSpeed: number;
+  highwayEfficiency: number;
+  avoids: boolean;
+  generalize: number;
+  shapeFormat: string;
+  unit: string;
+  locale: string;
+  useTraffic: boolean;
+  timeType: number;
+  dateType: number;
+  manMaps: boolean;
+  sideOfStreetDisplay: boolean;
+}
