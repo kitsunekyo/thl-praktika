2c554253c6aa700c3e75d2cac525e797adb7d42e
Alex Spieslechner <as@umdaschgroup-ventures.com>
2023-09-20 11:45:24 +0200

    allow setting address and tel in profile

--- src/app/(main)/profile/ProfileForm.tsx
+++ src/app/(main)/profile/ProfileForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { User } from "next-auth";
+import { User } from "@prisma/client";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -19,16 +19,28 @@ import { Input } from "@/components/ui/input";
 
 import { updateProfile } from "./actions";
 
+const phoneRegex = new RegExp(
+  /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/,
+);
+
 export const profileSchema = z.object({
   name: z.string(),
+  phone: z.string().regex(phoneRegex, "Ungültige Telefonnummer"),
+  address: z.string().optional(),
+  city: z.string().optional(),
+  zipCode: z.string().optional(),
 });
 
-export function ProfileForm({ user }: { user: User }) {
+export function ProfileForm({ user }: { user: Omit<User, "password"> }) {
   const [loading, setLoading] = useState(false);
   const form = useForm<z.infer<typeof profileSchema>>({
     resolver: zodResolver(profileSchema),
     defaultValues: {
       name: user.name || "",
+      phone: user.phone || "",
+      address: user.address || "",
+      city: user.city || "",
+      zipCode: user.zipCode || "",
     },
   });
 
@@ -49,13 +61,6 @@ export function ProfileForm({ user }: { user: User }) {
           },
         )}
       >
-        <FormItem>
-          <FormLabel>Email</FormLabel>
-          <FormControl>
-            <Input value={user.email || ""} disabled />
-          </FormControl>
-          <FormMessage />
-        </FormItem>
         <FormField
           control={form.control}
           name="name"
@@ -63,7 +68,66 @@ export function ProfileForm({ user }: { user: User }) {
             <FormItem>
               <FormLabel>Name</FormLabel>
               <FormControl>
-                <Input placeholder="Dein Name" {...field} />
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormItem>
+          <FormLabel>Email*</FormLabel>
+          <FormControl>
+            <Input value={user.email} disabled />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+        <FormField
+          control={form.control}
+          name="phone"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Telefon</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="address"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Adresse</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="zipCode"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Postleitzahl</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="city"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Stadt</FormLabel>
+              <FormControl>
+                <Input {...field} />
               </FormControl>
               <FormMessage />
             </FormItem>
--- src/app/(main)/profile/actions.ts
+++ src/app/(main)/profile/actions.ts
@@ -1,9 +1,10 @@
 "use server";
 
+import { User } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 
 import { getServerSession } from "@/lib/next-auth";
-import { prisma } from "@/lib/prisma";
+import { prisma, prismaExclude } from "@/lib/prisma";
 
 export async function getMe() {
   const session = await getServerSession();
@@ -18,15 +19,22 @@ export async function getMe() {
     },
     select: {
       id: true,
-      name: true,
       email: true,
+      name: true,
+      emailVerified: true,
       image: true,
       role: true,
+      address: true,
+      city: true,
+      zipCode: true,
+      phone: true,
     },
   });
 }
 
-export async function updateProfile(user: Partial<{ name: string }>) {
+export async function updateProfile(
+  user: Partial<Omit<User, "password" | "emailVerified" | "email" | "id">>,
+) {
   const session = await getServerSession();
 
   if (!session?.user) {
@@ -37,9 +45,7 @@ export async function updateProfile(user: Partial<{ name: string }>) {
     where: {
       id: session.user.id,
     },
-    data: {
-      name: user.name,
-    },
+    data: user,
   });
 
   revalidatePath("/profile");
