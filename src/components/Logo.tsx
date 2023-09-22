8b50a29264bc09d64cfa186cb330d96ba285f3ba
Alex Spieslechner <as@umdaschgroup-ventures.com>
2023-09-22 14:52:14 +0200

    make logo text black

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export function Logo({ onNavigate }: { onNavigate?: () => void }) {
   return (
-    <div className="spacing text-xs font-bold uppercase tracking-widest text-gray-400 hover:text-gray-500">
+    <div className="spacing text-xs font-bold uppercase tracking-widest hover:text-gray-500">
       <Link href="/" className="py-2" onClick={onNavigate}>
         THL Praktika
       </Link>
