279543bffdeadf362dd26dde87c622cf23449065
Alex Spieslechner <as@umdaschgroup-ventures.com>
2024-01-09 15:51:10 +0100

    fix: use exact on active sidebar nav for profile

--- src/app/(main)/Navigation.tsx
+++ src/app/(main)/Navigation.tsx
@@ -47,7 +47,7 @@ export function Navigation({
                 Meine Praktika
               </SidebarLink>
             )}
-            <SidebarLink href="/profile" onClick={onNavigate}>
+            <SidebarLink href="/profile" onClick={onNavigate} exact>
               <UserCircleIcon className="mr-2 h-4 w-4 shrink-0" />
               Mein Profil
             </SidebarLink>
