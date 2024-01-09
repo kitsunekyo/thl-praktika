7ef759f1d9f78493a7141acd2e34882ab95743a6
Alex Spieslechner <as@umdaschgroup-ventures.com>
2024-01-09 17:51:31 +0100

    feat: only return users in getUserProfiles

--- src/app/(main)/users/server.ts
+++ src/app/(main)/users/server.ts
@@ -4,6 +4,9 @@ import { prisma } from "@/lib/prisma";
 
 export async function getUserProfiles() {
   return await prisma.user.findMany({
+    where: {
+      role: "user",
+    },
     select: {
       id: true,
       name: true,
