95eb5284920291b27a71c14be4a37f0238602111
Alex Spieslechner <as@umdaschgroup-ventures.com>
2024-01-17 14:24:36 +0100

    refactor: abstract away email config

--- src/lib/postmark.ts
+++ src/lib/postmark.ts
@@ -18,28 +18,14 @@ export async function sendInvitationMail({
   name: string;
   role?: "user" | "trainer" | "admin";
 }) {
-  const payload = {
-    From: "hi@mostviertel.tech",
-    To: to,
-    TemplateAlias:
-      role === "trainer" ? "trainer-invitation" : "user-invitation",
-    MessageStream: "outbound",
-    TemplateModel: {
-      product_url: process.env.NEXTAUTH_URL,
-      name: name,
-      invite_sender_name: "Alex",
-      action_url: `${process.env.NEXTAUTH_URL}/signup?email=${to}&name=${name}&id=${id}`,
-    },
-  };
-  if (process.env.NODE_ENV === "production") {
-    try {
-      await client.sendEmailWithTemplate(payload);
-      return;
-    } catch (e) {
-      captureException(e);
-    }
-  }
-  console.log("mock: mail sent", payload);
+  const template =
+    role === "trainer" ? "trainer-invitation" : "user-invitation";
+  await sendMail(to, template, {
+    product_url: process.env.NEXTAUTH_URL,
+    name,
+    invite_sender_name: "Alex",
+    action_url: `${process.env.NEXTAUTH_URL}/signup?email=${to}&name=${name}&id=${id}`,
+  });
 }
 
 export async function sendTrainingRegistrationNotificationMail({
@@ -49,25 +35,10 @@ export async function sendTrainingRegistrationNotificationMail({
   to: string;
   trainerName: string;
 }) {
-  const payload = {
-    From: "hi@mostviertel.tech",
-    To: to,
-    TemplateAlias: "training-registration-notification",
-    MessageStream: "outbound",
-    TemplateModel: {
-      trainer_name: trainerName,
-      product_url: process.env.NEXTAUTH_URL,
-    },
-  };
-  if (process.env.NODE_ENV === "production") {
-    try {
-      await client.sendEmailWithTemplate(payload);
-      return;
-    } catch (e) {
-      captureException(e);
-    }
-  }
-  console.log("mock: mail sent", payload);
+  await sendMail(to, "training-registration-notification", {
+    trainer_name: trainerName,
+    product_url: process.env.NEXTAUTH_URL,
+  });
 }
 
 export async function sendTrainingRequestReceivedMail({
@@ -77,25 +48,10 @@ export async function sendTrainingRequestReceivedMail({
   to: string;
   userName: string;
 }) {
-  const payload = {
-    From: "hi@mostviertel.tech",
-    To: to,
-    TemplateAlias: "training-request-received",
-    MessageStream: "outbound",
-    TemplateModel: {
-      product_url: process.env.NEXTAUTH_URL,
-      user_name: userName,
-    },
-  };
-  if (process.env.NODE_ENV === "production") {
-    try {
-      await client.sendEmailWithTemplate(payload);
-      return;
-    } catch (e) {
-      captureException(e);
-    }
-  }
-  console.log("mock: mail sent", payload);
+  await sendMail(to, "training-request-received", {
+    product_url: process.env.NEXTAUTH_URL,
+    user_name: userName,
+  });
 }
 
 export async function sendForgotPasswordMail({
@@ -107,25 +63,10 @@ export async function sendForgotPasswordMail({
   tokenId: string;
   tokenValue: string;
 }) {
-  const payload = {
-    From: "hi@mostviertel.tech",
-    To: to,
-    TemplateAlias: "forgot-password",
-    MessageStream: "outbound",
-    TemplateModel: {
-      product_url: process.env.NEXTAUTH_URL,
-      action_url: `${process.env.NEXTAUTH_URL}/reset-password?t=${tokenId}&v=${tokenValue}`,
-    },
-  };
-  if (process.env.NODE_ENV === "production") {
-    try {
-      await client.sendEmailWithTemplate(payload);
-      return;
-    } catch (e) {
-      captureException(e);
-    }
-  }
-  console.log("mock: mail sent", payload);
+  await sendMail(to, "forgot-password", {
+    product_url: process.env.NEXTAUTH_URL,
+    action_url: `${process.env.NEXTAUTH_URL}/reset-password?t=${tokenId}&v=${tokenValue}`,
+  });
 }
 
 export async function sendTrainingCancelledMail({
@@ -137,28 +78,12 @@ export async function sendTrainingCancelledMail({
   trainer: string;
   date: string;
 }) {
-  const payload = {
-    From: "hi@mostviertel.tech",
-    To: to,
-    TemplateAlias: "training-cancelled",
-    MessageStream: "outbound",
-    TemplateModel: {
-      trainer,
-      date,
-      product_url: process.env.NEXTAUTH_URL,
-      action_url: process.env.NEXTAUTH_URL,
-    },
-  };
-  if (process.env.NODE_ENV === "production") {
-    try {
-      await client.sendEmailWithTemplate(payload);
-      return;
-    } catch (e) {
-      captureException(e);
-    }
-  }
-
-  console.log("mock: mail sent", payload);
+  await sendMail(to, "training-cancelled", {
+    trainer,
+    date,
+    product_url: process.env.NEXTAUTH_URL,
+    action_url: process.env.NEXTAUTH_URL,
+  });
 }
 
 export async function sendRegistrationCancelledMail({
@@ -172,29 +97,13 @@ export async function sendRegistrationCancelledMail({
   user: string;
   date: string;
 }) {
-  const payload = {
-    From: "hi@mostviertel.tech",
-    To: to,
-    TemplateAlias: "registration-cancelled",
-    MessageStream: "outbound",
-    TemplateModel: {
-      product_url: process.env.NEXTAUTH_URL,
-      user,
-      name: trainerName,
-      date,
-      action_url: process.env.NEXTAUTH_URL,
-    },
-  };
-  if (process.env.NODE_ENV === "production") {
-    try {
-      await client.sendEmailWithTemplate(payload);
-      return;
-    } catch (e) {
-      captureException(e);
-    }
-  }
-
-  console.log("mock: mail sent", payload);
+  await sendMail(to, "registration-cancelled", {
+    product_url: process.env.NEXTAUTH_URL,
+    user,
+    name: trainerName,
+    date,
+    action_url: process.env.NEXTAUTH_URL,
+  });
 }
 
 export async function sendTrainingUpdatedMail({
@@ -209,19 +118,47 @@ export async function sendTrainingUpdatedMail({
     "description" | "start" | "end" | "address" | "city" | "zipCode"
   >;
 }) {
+  await sendMail(to, "training-updated", {
+    trainer_name: trainerName,
+    date: formatTrainingDate(training.start, training.end),
+    description: training.description || "",
+    address: formatAddress(training),
+    product_url: process.env.NEXTAUTH_URL,
+    action_url: process.env.NEXTAUTH_URL,
+  });
+}
+
+export async function sendTrainingRegistrationMail({
+  to,
+  trainerName,
+  userName,
+  date,
+}: {
+  to: string;
+  trainerName: string;
+  userName: string;
+  date: string;
+}) {
+  await sendMail(to, "training-cancelled", {
+    name: trainerName,
+    user: userName,
+    date,
+    product_url: process.env.NEXTAUTH_URL,
+    action_url: process.env.NEXTAUTH_URL,
+  });
+}
+
+async function sendMail(
+  to: string,
+  templateAlias: string,
+  templateModel?: any,
+) {
   const payload = {
     From: "hi@mostviertel.tech",
     To: to,
-    TemplateAlias: "training-updated",
+    TemplateAlias: templateAlias,
     MessageStream: "outbound",
-    TemplateModel: {
-      trainer_name: trainerName,
-      date: formatTrainingDate(training.start, training.end),
-      description: training.description || "",
-      address: formatAddress(training),
-      product_url: process.env.NEXTAUTH_URL,
-      action_url: process.env.NEXTAUTH_URL,
-    },
+    TemplateModel: templateModel,
   };
   if (process.env.NODE_ENV === "production") {
     try {
@@ -231,6 +168,5 @@ export async function sendTrainingUpdatedMail({
       captureException(e);
     }
   }
-
   console.log("mock: mail sent", payload);
 }
