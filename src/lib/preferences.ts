689f1b7d951cd041e72f6cf6c3b00092e02d9dcc
Alex Spieslechner <as@umdaschgroup-ventures.com>
2024-02-02 14:55:40 +0100

    fix: handle null as input for user preferences

--- src/lib/preferences.ts
+++ src/lib/preferences.ts
@@ -1,24 +1,28 @@
 import { z } from "zod";
 
-export const preferencesSchema = z.object({
-  email: z
-    .object({
-      // trainer
-      trainingRequest: z.boolean().default(true),
-      trainingRegistration: z.boolean().default(true),
-      trainingRegistrationCancelled: z.boolean().default(true),
-      // user
-      trainingCreatedAfterRegistration: z.boolean().default(true),
-      trainingCancelled: z.boolean().default(true),
-      trainingUpdated: z.boolean().default(true),
-    })
-    .optional()
-    .default({
-      trainingRequest: true,
-      trainingRegistration: true,
-      trainingRegistrationCancelled: true,
-      trainingCreatedAfterRegistration: true,
-      trainingCancelled: true,
-      trainingUpdated: true,
-    }),
-});
+const defaultPreferences = {
+  email: {
+    trainingRequest: true,
+    trainingRegistration: true,
+    trainingRegistrationCancelled: true,
+    trainingCreatedAfterRegistration: true,
+    trainingCancelled: true,
+    trainingUpdated: true,
+  },
+};
+
+export const preferencesSchema = z
+  .object({
+    email: z
+      .object({
+        trainingRequest: z.boolean().default(true),
+        trainingRegistration: z.boolean().default(true),
+        trainingRegistrationCancelled: z.boolean().default(true),
+        trainingCreatedAfterRegistration: z.boolean().default(true),
+        trainingCancelled: z.boolean().default(true),
+        trainingUpdated: z.boolean().default(true),
+      })
+      .default({}),
+  })
+  .nullable()
+  .transform((data) => data || defaultPreferences);
