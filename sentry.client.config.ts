a8135daffc7e1b6e1eb8d6dcc3ee2fdfadc6ea79
Alex Spieslechner <as@umdaschgroup-ventures.com>
2024-02-19 08:52:01 +0100

    feat: add global-error for sentry

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -6,27 +6,9 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://98cbc3bab318d5ccd25dcd52abada7b8@o4505917837344768.ingest.sentry.io/4505917838589952",
-
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
-
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-
-  replaysOnErrorSampleRate: 1.0,
-
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
-
-  // You can remove this option if you're not planning to use the Sentry Session Replay feature:
-  integrations: [
-    new Sentry.Replay({
-      // Additional Replay configuration goes in here, for example:
-      maskAllText: true,
-      blockAllMedia: true,
-    }),
-  ],
-
   enabled: process.env.NODE_ENV === "production",
 });
--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -7,10 +7,8 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://98cbc3bab318d5ccd25dcd52abada7b8@o4505917837344768.ingest.sentry.io/4505917838589952",
-
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
-
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
   enabled: process.env.NODE_ENV === "production",
