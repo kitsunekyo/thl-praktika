f44948060e33ea5a688c89e103d50ee4cd97fcb5
Alex Spieslechner <alexander.spieslechner@gmail.com>
2024-02-29 15:50:32 +0100

    chore: version bump

--- src/app/(main)/changelog/layout.tsx
+++ src/app/(main)/changelog/layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Breadcrumbs>
-        <Breadcrumb href="/changelog">Änderungsverlauf</Breadcrumb>
+        <Breadcrumb href="/changelog">Änderungen</Breadcrumb>
       </Breadcrumbs>
       <article className="px-6 py-16 sm:px-8 md:py-32">
         <div className="prose mx-auto max-w-3xl prose-h1:text-5xl prose-h2:text-4xl prose-h3:text-xl">
