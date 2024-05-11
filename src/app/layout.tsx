600ab64995c0b9abbeada32416922163ca3a328e
Alex Spieslechner <alexander.spieslechner@gmail.com>
2024-05-11 09:38:21 +0200

    fix: try pwa splash without leading slash

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,92 +36,92 @@ export const metadata: Metadata = {
       {
         media:
           "screen and (width: 430px) and (height: 932px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 3)",
-        url: "/pwa/ios/splash_screens/iPhone_15_Pro_Max__iPhone_15_Plus__iPhone_14_Pro_Max_portrait.png",
+        url: "pwa/ios/splash_screens/iPhone_15_Pro_Max__iPhone_15_Plus__iPhone_14_Pro_Max_portrait.png",
       },
       {
         media:
           "screen and (width: 393px) and (height: 852px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 3)",
-        url: "/pwa/ios/splash_screens/iPhone_15_Pro__iPhone_15__iPhone_14_Pro_portrait.png",
+        url: "pwa/ios/splash_screens/iPhone_15_Pro__iPhone_15__iPhone_14_Pro_portrait.png",
       },
       {
         media:
           "screen and (width: 428px) and (height: 926px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 3)",
-        url: "/pwa/ios/splash_screens/iPhone_14_Plus__iPhone_13_Pro_Max__iPhone_12_Pro_Max_portrait.png",
+        url: "pwa/ios/splash_screens/iPhone_14_Plus__iPhone_13_Pro_Max__iPhone_12_Pro_Max_portrait.png",
       },
       {
         media:
           "screen and (width: 390px) and (height: 844px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 3)",
-        url: "/pwa/ios/splash_screens/iPhone_14__iPhone_13_Pro__iPhone_13__iPhone_12_Pro__iPhone_12_portrait.png",
+        url: "pwa/ios/splash_screens/iPhone_14__iPhone_13_Pro__iPhone_13__iPhone_12_Pro__iPhone_12_portrait.png",
       },
       {
         media:
           "screen and (width: 375px) and (height: 812px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 3)",
-        url: "/pwa/ios/splash_screens/iPhone_13_mini__iPhone_12_mini__iPhone_11_Pro__iPhone_XS__iPhone_X_portrait.png",
+        url: "pwa/ios/splash_screens/iPhone_13_mini__iPhone_12_mini__iPhone_11_Pro__iPhone_XS__iPhone_X_portrait.png",
       },
       {
         media:
           "screen and (width: 414px) and (height: 896px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 3)",
-        url: "/pwa/ios/splash_screens/iPhone_11_Pro_Max__iPhone_XS_Max_portrait.png",
+        url: "pwa/ios/splash_screens/iPhone_11_Pro_Max__iPhone_XS_Max_portrait.png",
       },
       {
         media:
           "screen and (width: 414px) and (height: 896px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 2)",
-        url: "/pwa/ios/splash_screens/iPhone_11__iPhone_XR_portrait.png",
+        url: "pwa/ios/splash_screens/iPhone_11__iPhone_XR_portrait.png",
       },
       {
         media:
           "screen and (width: 414px) and (height: 736px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 3)",
-        url: "/pwa/ios/splash_screens/iPhone_8_Plus__iPhone_7_Plus__iPhone_6s_Plus__iPhone_6_Plus_portrait.png",
+        url: "pwa/ios/splash_screens/iPhone_8_Plus__iPhone_7_Plus__iPhone_6s_Plus__iPhone_6_Plus_portrait.png",
       },
       {
         media:
           "screen and (width: 375px) and (height: 667px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 2)",
-        url: "/pwa/ios/splash_screens/iPhone_8__iPhone_7__iPhone_6s__iPhone_6__4.7__iPhone_SE_portrait.png",
+        url: "pwa/ios/splash_screens/iPhone_8__iPhone_7__iPhone_6s__iPhone_6__4.7__iPhone_SE_portrait.png",
       },
       {
         media:
           "screen and (width: 1032px) and (height: 1376px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 2)",
-        url: "/pwa/ios/splash_screens/13__iPad_Pro_M4_portrait.png",
+        url: "pwa/ios/splash_screens/13__iPad_Pro_M4_portrait.png",
       },
       {
         media:
           "screen and (width: 1024px) and (height: 1366px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 2)",
-        url: "/pwa/ios/splash_screens/12.9__iPad_Pro_portrait.png",
+        url: "pwa/ios/splash_screens/12.9__iPad_Pro_portrait.png",
       },
       {
         media:
           "screen and (width: 834px) and (height: 1210px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 2)",
-        url: "/pwa/ios/splash_screens/11__iPad_Pro_M4_portrait.png",
+        url: "pwa/ios/splash_screens/11__iPad_Pro_M4_portrait.png",
       },
       {
         media:
           "screen and (width: 834px) and (height: 1194px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 2)",
-        url: "/pwa/ios/splash_screens/11__iPad_Pro__10.5__iPad_Pro_portrait.png",
+        url: "pwa/ios/splash_screens/11__iPad_Pro__10.5__iPad_Pro_portrait.png",
       },
       {
         media:
           "screen and (width: 820px) and (height: 1180px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 2)",
-        url: "/pwa/ios/splash_screens/10.9__iPad_Air_portrait.png",
+        url: "pwa/ios/splash_screens/10.9__iPad_Air_portrait.png",
       },
       {
         media:
           "screen and (width: 834px) and (height: 1112px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 2)",
-        url: "/pwa/ios/splash_screens/10.5__iPad_Air_portrait.png",
+        url: "pwa/ios/splash_screens/10.5__iPad_Air_portrait.png",
       },
       {
         media:
           "screen and (width: 810px) and (height: 1080px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 2)",
-        url: "/pwa/ios/splash_screens/10.2__iPad_portrait.png",
+        url: "pwa/ios/splash_screens/10.2__iPad_portrait.png",
       },
       {
         media:
           "screen and (width: 768px) and (height: 1024px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 2)",
-        url: "/pwa/ios/splash_screens/9.7__iPad_Pro__7.9__iPad_mini__9.7__iPad_Air__9.7__iPad_portrait.png",
+        url: "pwa/ios/splash_screens/9.7__iPad_Pro__7.9__iPad_mini__9.7__iPad_Air__9.7__iPad_portrait.png",
       },
       {
         media:
           "screen and (width: 744px) and (height: 1133px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 2)",
-        url: "/pwa/ios/splash_screens/8.3__iPad_Mini_portrait.png",
+        url: "pwa/ios/splash_screens/8.3__iPad_Mini_portrait.png",
       },
     ],
   },
