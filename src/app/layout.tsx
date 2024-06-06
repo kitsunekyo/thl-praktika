659ac86f5a11dcab75452d5b23b6020a79d0319d
Alex Spieslechner <alexander.spieslechner@gmail.com>
2024-06-06 11:53:50 +0200

    feat: use local inter font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,18 +4,16 @@ import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { setDefaultOptions } from "date-fns";
 import { deAT } from "date-fns/locale";
 import type { Metadata, Viewport } from "next";
-import { Inter } from "next/font/google";
 import { Suspense } from "react";
 import { extractRouterConfig } from "uploadthing/server";
 
+import { inter } from "@/assets/fonts/inter";
 import { Loading } from "@/components/Loading";
 import PostHogPageView from "@/components/PostHogPageView";
 import { Providers } from "@/components/Providers";
 import { Toaster } from "@/components/ui/toaster";
 
 import { ourFileRouter } from "./api/uploadthing/core";
-
-const inter = Inter({ subsets: ["latin"] });
 
 setDefaultOptions({
   locale: deAT,
---
+++ src/assets/fonts/inter.ts
@@ -0,0 +1,51 @@
+import localFont from "next/font/local";
+
+export const inter = localFont({
+  src: [
+    {
+      path: "./inter-v13-latin-100.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "./inter-v13-latin-200.woff2",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "./inter-v13-latin-300.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "./inter-v13-latin-regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./inter-v13-latin-500.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./inter-v13-latin-600.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./inter-v13-latin-700.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./inter-v13-latin-800.woff2",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "./inter-v13-latin-900.woff2",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+});
